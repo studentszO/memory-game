37167046de90a475566fa60f09312ed4daf7e079
studentszO <169681673+studentszO@users.noreply.github.com>
2024-12-24 18:26:00 +0100

    fix: make app with style

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import './App.css'
+import "./App.css";
+import MakeGame from "./components/game/game";
 
 function App() {
-
   return (
     <>
-
+      <MakeGame />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
